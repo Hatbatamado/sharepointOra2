5e474b43945c669d3e8d0447f30010620db7f95e
Hatbatamado <nakiaaresz@gmail.com>
2015-03-30 15:04:27 +0200

    12 honap es honap szerinti napok, "random" szinekkel

---
+++ SolutionOra2.Web/Attekinto.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Attekinto.aspx.cs" Inherits="SolutionOra2.Web.Attekinto" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .piros {
+            background-color: red;
+            height:25px;
+            width:25px;
+            display:inline-block;
+            padding-right:5px;
+        }
+        .kek {
+            background-color: darkblue;
+            height:25px;
+            width:25px;
+            display:inline-block;
+            padding-right:5px;
+        }
+        .szurke {
+            background-color: gray;
+            height:25px;
+            width:25px;
+            display:inline-block;
+            padding-right:5px;
+        }
+        .honapNeve{
+            display:inline-block;
+            width:150px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Repeater runat="server" ID="KulsoRepeater">
+            <ItemTemplate>
+                <div class="honapNeve"><%# Eval("HonapNeve") %></div>
+                <asp:Repeater runat="server" ID="BelsoRepeater" DataSource='<%# Eval("BelsoLista") %>'>
+                    <ItemTemplate>
+                        <div runat="server" id="bejelentesKocka" class='<%# Eval("ElemOsztaly") %>'>
+                        </div>
+                    </ItemTemplate>
+                </asp:Repeater>
+                <br />
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SolutionOra2.Web/Attekinto.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SolutionOra2.Web.Models;
+
+namespace SolutionOra2.Web
+{
+    public partial class Attekinto : System.Web.UI.Page
+    {
+        public class AttekintoViewModel
+        {
+            public List<Elem> BelsoLista { get; set; }
+            public string HonapNeve { get; set; }
+
+            public AttekintoViewModel(int Honap)
+            {
+                BelsoLista = new List<Elem>();
+                for (int i = 1; i < DateTime.DaysInMonth(DateTime.Now.Year, Honap); i++)
+                {
+                    if (i % 2 == 0)
+                        BelsoLista.Add(new Elem() { ElemOsztaly = "piros", Statusz= Statusz.Elutasitva });
+                    else if (i % 3 == 0)
+                        BelsoLista.Add(new Elem() { ElemOsztaly = "szurke", Statusz = Statusz.Rogzitve });
+                    else
+                        BelsoLista.Add(new Elem() { ElemOsztaly = "kek", Statusz = Statusz.Jovahagyva });
+                }
+            }
+
+            public int Honap { get; set; }
+        }
+
+        public class Elem
+        {
+            public string ElemOsztaly { get; set; }
+            public Statusz Statusz { get; set; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTimeFormatInfo minfo = new DateTimeFormatInfo();
+            List<AttekintoViewModel> kulsoLista = new List<AttekintoViewModel>();
+            for (int i = 1; i <= 12; i++ )
+            {
+                kulsoLista.Add(new AttekintoViewModel(i) { HonapNeve = minfo.GetMonthName(i) });
+            }
+
+            KulsoRepeater.DataSource = kulsoLista;
+            KulsoRepeater.DataBind();
+        }
+    }
+}
